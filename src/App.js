bca77fa51bf27ef3fc13ef4c591defa8b42f8c70
Emerson Demetrio <emer.demetrio@gmail.com>
2019-06-10 13:09:34 -0300

    React Change

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Container from './container/Container';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<Container></Container>
+			</header>
+		</div>
+	);
+}
+
+export default App;
